e114b38f0495ab205ecaf77c9efea7995c9e6167
venmolom <p.widomski@gmail.com>
2021-11-11 15:19:05 +0100

    fix lint error

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
