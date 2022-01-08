2c66d173dfc5df7bd9de2c45894f595167423bd1
VenMolom <venmolom@protonmail.com>
2022-01-08 19:44:18 +0100

    add https conf

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ docker build -t one-click-desktop/admin-panel .
 # On running remember about:
 # 1. Passing to port 80 for http
 # 2. Passing to port 443 for https
+# 3. Adding volume for nginx config file
+# 4. Adding volume for ssl certificate
+# 5. Adding volume for ssl key
